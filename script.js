d88aceb01a3c409ef04be48503d7b8626a288b40
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-10-23 21:04:54 -0300

    🎉 Commit inicial

---
+++ script.js
@@ -0,0 +1,74 @@
+// console.log("Boa tarde!!")
+
+// var nome = "vini" <-- não utilizamos mais
+
+// let idade = 24
+
+// const rg = 1528632
+
+// console.log(rg)
+
+// string = tipo texto
+let texto = "20"
+console.log(typeof texto)
+// number = tipo numero
+let numero = 20
+console.log(numero)
+// boolean = true ou false
+let dia = true
+console.log(typeof dia)
+// undefined = não definido
+let aluno
+console.log(aluno)
+// null = nulo ou inexistente
+let tarde = null
+// NaN = não e um numero
+console.log(3 * "y")
+
+// if(condição){
+//   o retorno que sera passado
+// }
+
+let noite = false
+
+if(noite == true){
+    console.log("Você vai tomar banho")
+}else{
+    console.log("Você vai para escola")
+}
+
+let idade = 14
+
+if(idade >= 18){
+  console.log("Maior de idade")
+} else if (idade == 15){
+    console.log("Está quase lá")    
+} else if (idade){
+
+}else{
+    console.log("Não pode acessar o site!")
+}
+
+// let nota = prompt("Digite sua nota da prova")
+
+// if(nota >= 6){
+//    alert("Você foi Aprovado!!")
+// }else{
+//     alert("Reprovado")
+// }
+
+// operadores de comparação
+
+// = atribuição de tipo
+// == verificação de valor
+// === verificação de tipo e valor
+// != verifica se os valores comparados são diferentes
+// !== verifica se o valor e o tipo comparado é diferente
+// > maior que
+// < menor que
+// >= maior ou igual
+// <= menor ou igual
+
+let sim = 5
+let nao = "3"
+console.log(nao <= sim)
